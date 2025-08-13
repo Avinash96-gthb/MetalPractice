3bd7a7793b9d7f941bf08d4691fc9f38d109bc42
Avinash96-gthb <avinash.a2k4@gmail.com>
2025-08-13 10:23:23 +0530

    init commit

--- MetalPractice/ViewController.swift
+++ MetalPractice/ViewController.swift
@@ -6,13 +6,41 @@
 //
 
 import Cocoa
+import Metal
+import MetalKit
 
-class ViewController: NSViewController {
-
+class ViewController: NSViewController, MTKViewDelegate {
+    
+    
+    @IBOutlet weak var metalView: MTKView!
+    var device: MTLDevice!
+    var commandQueu: MTLCommandQueue!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        device = MTLCreateSystemDefaultDevice()
+        metalView.device = device
+        metalView.clearColor = MTLClearColor(red: 0, green: 0.1, blue: 1, alpha: 1)
+        metalView.delegate = self
+        commandQueu = device.makeCommandQueue()
 
         // Do any additional setup after loading the view.
+    }
+    
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+        
+    }
+    
+    func draw(in view: MTKView) {
+        let commandBuffer = commandQueu.makeCommandBuffer()
+        guard let renderPassDescriptor = view.currentRenderPassDescriptor else {
+            print("error lol dropping frames")
+            return
+        }
+        let renderPassEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
+        renderPassEncoder?.endEncoding()
+        commandBuffer?.present(view.currentDrawable!)
+        commandBuffer?.commit()
     }
 
     override var representedObject: Any? {
