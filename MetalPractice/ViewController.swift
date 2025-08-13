5ce8fae470f9f512979572e38596c28ce01afe2f
Avinash96-gthb <avinash.a2k4@gmail.com>
2025-08-13 09:32:04 +0530

    Initial Commit

---
+++ MetalPractice/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MetalPractice
+//
+//  Created by A Avinash Chidambaram on 13/08/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
